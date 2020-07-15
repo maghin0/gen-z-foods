3ef30c20c8e2564dcec975db2c14efe038a05eb3
Tarek Maghin <ahmedshabana0.0@gmail.com>
2020-07-15 22:49:57 +0200

    video and pricing title

--- src/components/capabilities/Capabilities.js
+++ src/components/capabilities/Capabilities.js
@@ -13,6 +13,7 @@ const Capabilities = () =>
      
 
 Big portals like Otlob, Uber Eats, and Glovo take a hefty percentage of your online sales revenue (20-30%) which means the more you sell online the more money they’ll take from you. With gen z foods, you’ll pay a flat monthly subscription (only 750 EGP) no matter how many online sales you make, which means you won’t be punished for selling more online! The more sales you make the more money you’ll keep!
+<br />
 <p>Here’s an example to show you how much more money you can make with gen z foods online ordering system. If you have an average order of 50 EGP, and you sell an average of 20 online orders per day, that means you’ll have a revenue of 30,000 EGP. If you’re making those sales on platforms such as Otlob or Uber Eats, you’ll be giving them 9,000 EGP, leaving you with 21,000 EGP. However, if you’re making those online sales with gen z foods, you’ll only give us a flat monthly fee of 750 EGP, and keep 29,250 EGP. Which means you get to make 8,250 EGP more with gen z foods! </p>
 <br />
 <br/>
@@ -31,6 +32,9 @@ Big portals like Otlob, Uber Eats, and Glovo take a hefty percentage of your onl
 
 <br />
       <h2 id="two"> Our Services</h2>
+      <h2 >
+      Only < style={{color: "#041836"}}> 750EGP/month</a> for all our services!
+      </h2>
       <div className="features">
       
         <CapabilityList />
